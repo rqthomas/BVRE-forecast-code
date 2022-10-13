3598bb060d8f3f7cf4c37a186b16fbf1174eed43
Heather Wander <hwander@vt.edugit config --global user.name Heather>
2022-10-13 12:30:57 -0400

    updating files so they work with new FLAREr updates

--- workflows/default/03_run_flarer_forecast.R
+++ workflows/default/03_run_flarer_forecast.R
@@ -9,16 +9,18 @@ if(file.exists("~/.aws")){
 Sys.setenv("AWS_DEFAULT_REGION" = "s3",
            "AWS_S3_ENDPOINT" = "flare-forecast.org")
 
-#code to delete restart config file - useful when running multiple different forecast horizons instead of consecutive forecast days
-unlink(file.path(getwd(),"restart/bvre/bvre_test/configure_run.yml"))
-
 lake_directory <- here::here()
 update_run_config <- TRUE
 config_set_name <- "default"
+files.sources <- list.files(file.path(lake_directory, "R"), full.names = TRUE)
+sapply(files.sources[grepl(".R$", files.sources)], source)
 
 configure_run_file <- "configure_run.yml"
 
 config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
+config$model_settings$model <- "GLM"
+config$run_config$sim_name <- "test"
+config$run_config$use_s3 <- FALSE
 
 config <- FLAREr::get_restart_file(config, lake_directory)
 
@@ -27,13 +29,9 @@ FLAREr::get_targets(lake_directory, config)
 noaa_forecast_path <- FLAREr::get_driver_forecast_path(config,
                                                forecast_model = config$met$forecast_met_model)
 
-inflow_forecast_path <- FLAREr::get_driver_forecast_path(config,
-                                                 forecast_model = config$inflow$forecast_inflow_model)
-#inflow_forecast_path <- NULL
-
-pars_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$par_config_file), col_types = readr::cols())
-obs_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$obs_config_file), col_types = readr::cols())
-states_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$states_config_file), col_types = readr::cols())
+#inflow_forecast_path <- FLAREr::get_driver_forecast_path(config,
+#                                                 forecast_model = config$inflow$forecast_inflow_model)
+inflow_forecast_path <- NULL
 
 if(!is.null(noaa_forecast_path)){
   FLAREr::get_driver_forecast(lake_directory, forecast_path = noaa_forecast_path)
@@ -42,21 +40,34 @@ if(!is.null(noaa_forecast_path)){
   forecast_dir <- NULL
 }
 
+if(!is.null(inflow_forecast_path)){
+  FLAREr::get_driver_forecast(lake_directory, forecast_path = inflow_forecast_path)
+  inflow_file_dir <- file.path(config$file_path$noaa_directory,inflow_forecast_path)
+}else{
+  inflow_file_dir <- NULL
+}
+
+pars_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$par_config_file), col_types = readr::cols())
+pars_config <- pars_config[pars_config$model == config$model_settings$model, ]
+obs_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$obs_config_file), col_types = readr::cols())
+states_config <- readr::read_csv(file.path(config$file_path$configuration_directory, config$model_settings$states_config_file), col_types = readr::cols())
+
 #Download and process observations (already done)
-
-FLAREr::get_stacked_noaa(lake_directory, config, averaged = TRUE)
-
-met_out <- FLAREr::generate_glm_met_files(obs_met_file = file.path(config$file_path$noaa_directory, "noaa", "NOAAGEFS_1hr_stacked_average", config$location$site_id, paste0("observed-met-noaa_",config$location$site_id,".nc")),
+met_out <- FLAREr::generate_glm_met_files(obs_met_file = file.path(config$file_path$qaqc_data_directory, paste0("observed-met_",config$location$site_id,".nc")),
                                           out_dir = config$file_path$execute_directory,
                                           forecast_dir = forecast_dir,
                                           config = config)
 
 met_out$filenames <- met_out$filenames[!stringr::str_detect(met_out$filenames, "ens00")]
 
-#met_out <- FLAREr::generate_glm_met_files(obs_met_file = file.path(config$file_path$qaqc_data_directory, paste0("observed-met_",config$location$site_id,".nc")),
-#                                          out_dir = config$file_path$execute_directory,
-#                                          forecast_dir = forecast_dir,
-#                                          config = config)
+if(config$model_settings$model_name == "glm_aed"){
+  
+  https_file <- "https://raw.githubusercontent.com/cayelan/FCR-GLM-AED-Forecasting/master/FCR_2013_2019GLMHistoricalRun_GLMv3beta/inputs/FCR_SSS_inflow_2013_2021_20211102_allfractions_2DOCpools.csv"
+  download.file(https_file,
+                file.path(config$file_path$execute_directory, basename(https_file)))
+  
+  inflow_outflow_files$inflow_file_name <- cbind(inflow_outflow_files$inflow_file_name, rep(file.path(config$file_path$execute_directory,basename(https_file)), length(inflow_outflow_files$inflow_file_name)))
+}
 
 #Create observation matrix
 obs <- FLAREr::create_obs_matrix(cleaned_observations_file_long = file.path(config$file_path$qaqc_data_directory, paste0(config$location$site_id, "-targets-insitu.csv")),
@@ -112,17 +123,6 @@ rm(da_forecast_output)
 gc()
 
 FLAREr::update_run_config(config, lake_directory, configure_run_file, saved_file, new_horizon = 35, day_advance = 1, new_start_datetime = TRUE)
-
-setwd(lake_directory)
-unlink(config$run_config$restart_file)
-unlink(forecast_dir, recursive = TRUE)
-unlink(file.path(lake_directory, "flare_tempdir", config$location$site_id, config$run_config$sim_name), recursive = TRUE)
-
-
-setwd(lake_directory)
-unlink(config$run_config$restart_file)
-unlink(forecast_dir, recursive = TRUE)
-unlink(file.path(lake_directory, "flare_tempdir", config$location$site_id, config$run_config$sim_name), recursive = TRUE)
 
 message(paste0("successfully generated flare forecats for: ", basename(saved_file)))
 
