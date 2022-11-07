9024454ca3368668c99115c672234ff0115ddc43
rqthomas <rqthomas@vt.edu>
2022-11-07 08:59:28 -0500

    quick arrow based analysis

---
+++ workflows/DA_experiments/example_analysis.R
@@ -0,0 +1,15 @@
+score_dir <- arrow::SubTreeFileSystem$create(file.path(lake_directory,"scores/da_study"))
+scores <- arrow::open_dataset(score_dir) |> collect()
+
+
+rmse <- scores |> 
+  filter(!is.na(observation),
+         variable == "temperature",
+         horizon > 0) |> 
+  mutate(sq_error = (mean - observation)^2) |> 
+  group_by(horizon, model_id) |> 
+  summarize(rmse = sqrt(mean(sq_error, na.rm = TRUE)), .groups = "drop")
+
+
+ggplot(rmse, aes(x = horizon, y = rmse, color = model_id)) +
+  geom_line()
