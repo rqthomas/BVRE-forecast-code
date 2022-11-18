dc738f15698f0a19e1979232a94b5a7a5c6fc4a6
rqthomas <rqthomas@vt.edu>
2022-11-18 15:36:44 -0500

    cleaning up

--- workflows/default/01_generate_targets.R
+++ workflows/default/01_generate_targets.R
@@ -18,6 +18,9 @@ sapply(files.sources[grepl(".R$", files.sources)], source)
 #' Generate the `config_obs` object and create directories if necessary
 
 config_obs <- FLAREr::initialize_obs_processing(lake_directory, observation_yml = "observation_processing.yml", config_set_name = config_set_name)
+config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
+
+
 dir.create(file.path(lake_directory, "targets", config_obs$site_id), showWarnings = FALSE)
 #' Clone or pull from data repositories
 
@@ -51,13 +54,11 @@ cleaned_insitu_file <- in_situ_qaqc(insitu_obs_fname = file.path(lake_directory,
 
 message("Successfully generated targets")
 
-config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
-
-
 FLAREr::put_targets(site_id = config_obs$site_id,
                     cleaned_insitu_file,
-                    cleaned_met_file,
-                    use_s3 = TRUE,
+                    cleaned_met_file = NA,
+                    cleaned_inflow_file = NA,
+                    use_s3 = config$run_config$use_s3,
                     config = config)
 
 message("Successfully moved targets to s3 bucket")
